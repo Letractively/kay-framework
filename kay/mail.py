fe5c1ad10fba18282e659e749d2935ff666e0b7b
Takashi Matsuo <tmatsuo@candit.jp>
2009-06-28 17:37:17 +0900

    On the half way of writting the forms document.

---
+++ kay/mail.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+"""
+Tools for sending email.
+
+:copyright: (c) 2009 by Kay Team, see AUTHORS for more details.
+:license: BSD, see LICENSE for more details.
+"""
+
+#TODO: use local.app.app_settings.ADMINS instead of kay.conf.settings
+
+from google.appengine.api import mail
+
+from kay.conf import settings
+
+def mail_admins(subject, message, fail_silently=False):
+  """Sends a message to the admins, as defined by the ADMINS setting."""
+  if not settings.ADMINS:
+    return
+  for admin in settings.ADMINS:
+    mail.send_mail(sender=admin[1],
+                   to=admin[1],
+                   subject=subject,
+                   body=message)
