1b321ff5b0b793f1f51c809c403e58cf192b76e9
Ian Lewis <IanMLewis@gmail.com>
2011-03-03 20:40:17 +0900

    Updated version info for 1.1 rc2 release

--- kay/__init__.py
+++ kay/__init__.py
@@ -16,8 +16,8 @@ import logging
 
 import settings
 
-__version__ = "1.1.0rc1"
-__version_info__ = (1, 1, 0, 'rc', 1)
+__version__ = "1.1.0rc2"
+__version_info__ = (1, 1, 0, 'rc', 2)
 
 KAY_DIR = os.path.abspath(os.path.dirname(__file__))
 LIB_DIR = os.path.join(KAY_DIR, 'lib')
