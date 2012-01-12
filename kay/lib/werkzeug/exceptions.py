0c9646bac13e6ef023fe974fa72eec89148b6cf0
Takashi Matsuo <matsuo.takashi@gmail.com>
2012-01-12 16:44:01 +0900

    Upgrade werkzeug to 0.9-dev. (39c6af65fd4c3b42ef1fdaebbe16497114e776c5)

--- kay/lib/werkzeug/exceptions.py
+++ kay/lib/werkzeug/exceptions.py
@@ -12,7 +12,8 @@
 
     ::
 
-        from werkzeug import BaseRequest, responder
+        from werkzeug.wrappers import BaseRequest
+        from werkzeug.wsgi import responder
         from werkzeug.exceptions import HTTPException, NotFound
 
         def view(request):
@@ -53,7 +54,7 @@
                 return e
 
 
-    :copyright: (c) 2010 by the Werkzeug Team, see AUTHORS for more details.
+    :copyright: (c) 2011 by the Werkzeug Team, see AUTHORS for more details.
     :license: BSD, see LICENSE for more details.
 """
 import sys
@@ -153,22 +154,6 @@ class HTTPException(Exception):
     def __repr__(self):
         return '<%s \'%s\'>' % (self.__class__.__name__, self)
 
-    @property
-    def status_code(self):
-        # This property is added for more robust consistency between
-        # BaseResponse and HTTPException.
-
-        # DEPRECATED 1.1
-        # This code is deprecated and will be removed in
-        # versions >= 1.5 or 2.0
-        from kay.conf import settings
-        if settings.DEBUG:
-            import logging
-            logging.debug("Deprecation warning. The status_code property on "
-                         "werkzeug HTTPException objects is not part of the "
-                         "standard werkzeug distribution and may be removed in a "
-                         "future version of kay.")
-        return self.code
 
 class _ProxyException(HTTPException):
     """An HTTP exception that expands renders a WSGI application on error."""
@@ -192,6 +177,21 @@ class BadRequest(HTTPException):
         '<p>The browser (or proxy) sent a request that this server could '
         'not understand.</p>'
     )
+
+
+class ClientDisconnected(BadRequest):
+    """Internal exception that is raised if Werkzeug detects a disconnected
+    client.  Since the client is already gone at that point attempting to
+    send the error message to the client might not work and might ultimately
+    result in another exception in the server.  Mainly this is here so that
+    it is silenced by default as far as Werkzeug is concerned.
+
+    Since disconnections cannot be reliably detected and are unspecified
+    by WSGI to a large extend this might or might not be raised if a client
+    is gone.
+
+    .. versionadded:: 0.8
+    """
 
 
 class Unauthorized(HTTPException):
@@ -294,6 +294,21 @@ class RequestTimeout(HTTPException):
     )
 
 
+class Conflict(HTTPException):
+    """*409* `Conflict`
+
+    Raise to signal that a request cannot be completed because it conflicts
+    with the current state on the server.
+
+    .. versionadded:: 0.7
+    """
+    code = 409
+    description = (
+        '<p>A conflict happened while processing the request.  The resource '
+        'might have been modified while the request was being processed.'
+    )
+
+
 class Gone(HTTPException):
     """*410* `Gone`
 
@@ -367,6 +382,47 @@ class UnsupportedMediaType(HTTPException):
     description = (
         '<p>The server does not support the media type transmitted in '
         'the request.</p>'
+    )
+
+
+class RequestedRangeNotSatisfiable(HTTPException):
+    """*416* `Requested Range Not Satisfiable`
+
+    The client asked for a part of the file that lies beyond the end
+    of the file.
+
+    .. versionadded:: 0.7
+    """
+    code = 416
+    description = (
+        '<p>The server cannot provide the requested range.'
+    )
+
+
+class ExpectationFailed(HTTPException):
+    """*417* `Expectation Failed`
+
+    The server cannot meet the requirements of the Expect request-header.
+
+    .. versionadded:: 0.7
+    """
+    code = 417
+    description = (
+        '<p>The server could not meet the requirements of the Expect header'
+    )
+
+
+class ImATeapot(HTTPException):
+    """*418* `I'm a teapot`
+
+    The server should return this if it is a teapot and someone attempted
+    to brew coffee with it.
+
+    .. versionadded:: 0.7
+    """
+    code = 418
+    description = (
+        '<p>This server is a teapot, not a coffee machine'
     )
 
 
@@ -469,6 +525,11 @@ class Aborter(object):
         raise self.mapping[code](*args, **kwargs)
 
 abort = Aborter()
+
+
+#: an exception that is used internally to signal both a key error and a
+#: bad request.  Used by a lot of the datastructures.
+BadRequestKeyError = BadRequest.wrap(KeyError)
 
 
 # imported here because of circular dependencies of werkzeug.utils
