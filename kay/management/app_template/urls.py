e6f88e8bea4560d45a419ac81d66f9d87fd7202a
Paolo Casciello <paolo.casciello@gmail.com>
2012-01-14 19:18:58 +0100

    Removed the obsolete method for url routing in the app tenplate

--- kay/management/app_template/urls.py
+++ kay/management/app_template/urls.py
@@ -2,22 +2,6 @@
 # %app_name%.urls
 # 
 
-# Following few lines is an example urlmapping with an older interface.
-"""
-from werkzeug.routing import EndpointPrefix, Rule
-
-def make_rules():
-  return [
-    EndpointPrefix('%app_name%/', [
-      Rule('/', endpoint='index'),
-    ]),
-  ]
-
-all_views = {
-  '%app_name%/index': '%app_name%.views.index',
-}
-"""
-
 from kay.routing import (
   ViewGroup, Rule
 )
