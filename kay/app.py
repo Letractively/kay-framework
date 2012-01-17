9bccbc0ca7fb57e48941e0ce326c4faf70044eba
Takashi Matsuo <matsuo.takashi@gmail.com>
2012-01-17 01:32:55 -0800

    Suppressed warnings on local unit tests.

--- kay/app.py
+++ kay/app.py
@@ -465,7 +465,9 @@ class KayApp(object):
     local.url_adapter = self.url_map.bind_to_environ(environ)
 
   def __call__(self, environ, start_response):
-    if self.app_settings.DEBUG:
+    if self.app_settings.DEBUG and \
+      'SERVER_SOFTWARE' in os.environ and \
+      not os.environ['SERVER_SOFTWARE'].startswith('Test'):
       logging.getLogger().setLevel(logging.DEBUG)
     self._prepare(environ)
 
