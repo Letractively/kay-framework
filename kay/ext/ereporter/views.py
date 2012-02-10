82d4c720ea5c646e25bae6b8ef05c2063f8126b3
Takashi Matsuo <matsuo.takashi@gmail.com>
2012-02-10 11:43:24 -0800

    Add warning.

--- kay/ext/ereporter/views.py
+++ kay/ext/ereporter/views.py
@@ -162,7 +162,8 @@ def report_admin(request):
     exceptions = GetQuery(order='-date', **query_args)
     paginator = Paginator(exceptions, 10)
     page = paginator.page(request.args.get('page', 1))
-  except db.NeedIndexError:
+  except db.NeedIndexError, e:
+    logging.warn(e)
     exceptions = GetQuery(**query_args)
     paginator = Paginator(exceptions, 10)
     page = paginator.page(request.args.get('page', 1))
